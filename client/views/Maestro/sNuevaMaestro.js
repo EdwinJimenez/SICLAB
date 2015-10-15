55cba270f8e3f6bf231f20c940d7761dd4da402d
judilu <judilu_12@hotmail.com>
2015-10-15 10:43:08 -0600

    eleccion materiales maestro

--- client/views/Maestro/sNuevaMaestro.js
+++ client/views/Maestro/sNuevaMaestro.js
@@ -1,7 +1,15 @@
- Template.sNuevaMaestro.onRendered(function(){
+Template.sNuevaMaestro.onRendered(function(){
     $('select').material_select();
     $('.datepicker').pickadate({
     selectMonths: true, 
     selectYears: 15
-  	});
-  });
+  	}),
+  	$("#principal").show();
+	$("#eleccionMaterial").hide();
+ });
+Template.sNuevaMaestro.events({
+	"click #btnElegirMaterial":function(event,template){
+		$("#principal").hide();
+		$("#eleccionMaterial").show("slow");
+	}
+});
