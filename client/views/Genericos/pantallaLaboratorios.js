272b49056b28e1a75899bfa29a7bb7f83fe98904
judilu <judilu_12@hotmail.com>
2015-10-21 16:31:54 -0600

    Pantallas Genericas (ocultar templates)

--- client/views/Genericos/pantallaLaboratorios.js
+++ client/views/Genericos/pantallaLaboratorios.js
@@ -1,6 +1,13 @@
 Template.pantallaLaboratorios.onRendered(function(){
 	$('ul.tabs').tabs();
 	$("#solicitudesPendientes").show("slow");
+	$("#pantallaInventario").show("slow");
+	$("#altaArticulos").hide();
+	$("#bajaArticulos").hide();
+	$("#peticionesPendientes").hide();
+	$("#peticionesArticulos").hide();
+	$("#sPendientesLab").show("slow");
+	$("#sAceptadasLab").hide();
 });
 Template.pantallaLaboratorios.events({
 	"click #btnPendientes":function(event,Template){
@@ -10,5 +17,48 @@ Template.pantallaLaboratorios.events({
 	"click #btnEnProceso":function(event,Template){
 		$("#solicitudesPendientes").hide("slow");
 		$("#solicitudesEnProceso").show("slow");
+	},
+	"click #btnArticulos": function(event, Template){
+		$("#pantallaInventario").show("slow");
+		$("#altaArticulos").hide();
+		$("#bajaArticulos").hide();
+		$("#peticionesPendientes").hide();
+		$("#peticionesArticulos").hide();
+	},
+	"click #btnAlta": function(event, Template){
+		$("#pantallaInventario").hide();
+		$("#altaArticulos").show("slow");
+		$("#bajaArticulos").hide();
+		$("#peticionesPendientes").hide();
+		$("#peticionesArticulos").hide();
+	},
+	"click #btnBaja": function(event, Template){
+		$("#pantallaInventario").hide();
+		$("#altaArticulos").hide();
+		$("#bajaArticulos").show("slow");
+		$("#peticionesPendientes").hide();
+		$("#peticionesArticulos").hide();
+	},
+	"click #btnPeticionesPendientes": function(event, Template){
+		$("#pantallaInventario").hide();
+		$("#altaArticulos").hide();
+		$("#bajaArticulos").hide();
+		$("#peticionesPendientes").show("slow");
+		$("#peticionesArticulos").hide();
+	},
+	"click #btnPeticiónArticulo": function(event, Template){
+		$("#pantallaInventario").hide();
+		$("#altaArticulos").hide();
+		$("#bajaArticulos").hide();
+		$("#peticionesPendientes").hide();
+		$("#peticionesArticulos").show("slow");
+	},
+	"click #btnPendientesLab": function(event,Template){
+		$("#sPendientesLab").show("slow");
+		$("#sAceptadasLab").hide();
+	},
+	"click #btnAceptadasLab": function(event, Template){
+		$("#sPendientesLab").hide();
+		$("#sAceptadasLab").show("slow");
 	}
 });
