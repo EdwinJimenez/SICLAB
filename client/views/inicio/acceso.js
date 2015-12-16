d8becab44f3c918a004ccfa8ea5ef47de28a1527
AnaPatriciaL <paty.lopezvalenzuela@gmail.com>
2015-12-16 12:06:44 -0700

    usuarios

--- client/views/inicio/acceso.js
+++ client/views/inicio/acceso.js
@@ -3,15 +3,40 @@ Template.acceso.events({
 		if(Meteor.validaciones.validarVacios(acceso))
 		{
 			if($("#txtUsuario").val()=='Laboratorio')
-			Router.go("/Alumno");
-		else{
-			if($("#txtUsuario").val()=='Maestro')
-				Router.go("/pantallaMaestro");
-			else
-				if($("#txtUsuario").val()=='Encargado')
-				Router.go("/pantallaLaboratorios");
-			}
+				Router.go("/Alumno");
+			else{
+				if($("#txtUsuario").val()=='Maestro')
+					Router.go("/pantallaMaestro");
+				else
+					if($("#txtUsuario").val()=='jefeLab'){
+						Router.go("/pantallaLaboratorios");
+						Session.set("tipoUsuario",1);
+					}
+					else
+					{
+						if($("#txtUsuario").val()=='jefeDepto'){
+							Router.go("/pantallaLaboratorios");
+							Session.set("tipoUsuario",2);
+						}
+						else
+						{
+							if($("#txtUsuario").val()=='encargadoLab'){
+								Router.go("/pantallaLaboratorios");
+								Session.set("tipoUsuario",3);
+							}
+							else
+							{
+								if($("#txtUsuario").val()=='ss'){
+									Router.go("/pantallaLaboratorios");
+									Session.set("tipoUsuario",4);
+								}
+							}
 
+						}
+					}
+
+				}
+
+			}
 		}
-	}
-});
+	});
